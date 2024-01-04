64b03a870e87cbed05483fd472043f17f598dddc
Eduardo Wagner <eduardoodelonw@gmail.com>
2024-01-04 18:02:51 -0300

    feat(backend): initial backend setup

---
+++ dev/backend/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: process.env.ORIGIN ?? '*',
+    credentials: true,
+  });
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder().setTitle('iHeros API').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3333);
+}
+bootstrap();
